9926831610f4b56beea62ac79f3bf62490c6892b
fanzeyu <fanzeyu@2b16725b-f4d1-cf46-9168-34b0a6dc7cd7>
2018-02-05 10:13:55 +0000

    页面修改

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
 </script>
 
 <style>
+  @import url("https://at.alicdn.com/t/font_564746_qew1uk3n1rk3ayvi.css");
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
